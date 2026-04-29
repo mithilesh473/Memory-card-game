5376e6205a32159d73dc29067454a9d2b170051a
mithilesh473 <vaddimithilesh@gmail.com>
2026-04-29 13:49:59 +0530

    added all the functionalities and created a custom hook for cleaning App.jsx

--- vite-project/src/components/GameHeader.jsx
+++ vite-project/src/components/GameHeader.jsx
@@ -1,6 +1,6 @@
 
 
-export const GameHeader=({score,moves})=>{
+export const GameHeader=({score,moves,onReset})=>{
     return (
     <div className="game-header">
         <h1>🎮Memory Card Game</h1>
@@ -14,6 +14,7 @@ export const GameHeader=({score,moves})=>{
                 <span className="stat-value">{moves}</span> 
             </div>
         </div>
+        <button className="reset-btn" onClick={onReset}>New Game</button>
     </div>
     )
 }
---
+++ vite-project/src/hooks/useGameLogic.js
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+
+
+
+export const useGameLogic=(cardValues)=>{
+    const [cards, setCards] = useState([]);
+      const [flippedCards, setFlippedCards] = useState([]);
+      const [matchedCards, setMatchedCards] = useState([]);
+      const [score,setScore]=useState(0);
+      const [moves,setMoves]=useState(0);
+      const[isLocked,setIsLocked]=useState(false)
+    
+      const shuffleArray=(array)=>{
+        const shuffled=[...array];
+        for(let i =shuffled.length-1;i>0;i--){
+          const j=Math.floor(Math.random()*(i+1));
+          [shuffled[i],shuffled[j]]=[shuffled[j],shuffled[i]];
+        }
+        return shuffled;
+      }
+    
+      const initalizeGame = () => {
+    
+        // shuffle the cards
+        
+        const shuffled=shuffleArray(cardValues);
+    
+    
+        const finalCards = cardValues.map((value, index) => ({
+          id: index,
+          value,
+          isFlipped: false,
+          isMatched: false,
+          
+        }));
+          setScore(0);
+          setMoves(0);
+          setIsLocked(false);
+          setMatchedCards([]);
+          setFlippedCards([]);
+    
+        setCards(finalCards);
+      };
+    
+      useEffect(() => {
+        initalizeGame();
+      }, []);
+    
+      const handleCardClick = (card) => {
+        // Don't allow clicking if card is already flipped or matched
+        if (card.isFlipped || card.isMatched || isLocked||flippedCards.length===2) {
+          return;
+        }
+    
+        // Update the card flipped state
+        const newCards = cards.map((c) => {
+          if (c.id === card.id) {
+            return { ...c, isFlipped: true };
+          } else {
+            return c;
+          }
+        });
+    
+        setCards(newCards);
+    
+        const newFlippedCards = [...flippedCards, card.id];
+        setFlippedCards(newFlippedCards);
+    
+        // check if two cards are flipped
+        if (flippedCards.length === 1) {
+          setIsLocked(true);
+          const firstCard = cards[flippedCards[0]];
+    
+          if (firstCard.value === card.value) {
+            setTimeout(() => {
+              setMatchedCards((prev) => [...prev, firstCard.id, card.id]);
+    
+              setScore((prev)=>prev+1);
+    
+              setCards((prev)=>prev.map((c) => {
+                if (c.id === card.id || c.id ===firstCard.id) {
+                  return { ...c, isMatched: true };
+                } else {
+                  return c;
+                }
+              }));
+              setFlippedCards([]);
+              setIsLocked(false);
+            },500);
+          } else {
+            // flip back card 1, card 2
+            setTimeout(() => {
+              const flippedBackCard = newCards.map((c) => {
+                if (newFlippedCards.includes(c.id) || c.id === card.id) {
+                  return { ...c, isFlipped: false };
+                } else {
+                  return c;
+                }
+              });
+    
+              setCards(flippedBackCard);
+              setIsLocked(false);
+              setFlippedCards([]);
+            }, 1000);
+          }
+          setMoves((prev)=>prev+1);
+    
+        }
+      };
+      const isGameComplete=matchedCards.length===cardValues.length;
+      return{cards,score,moves,isGameComplete,initalizeGame,handleCardClick}
+    
+}
